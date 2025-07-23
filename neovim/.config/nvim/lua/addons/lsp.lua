00f7cfe968382b25e35bf8cf21f6d7b4219a3fe6
keyboard-slayer <git@keyb.moe>
2025-07-23 18:17:55 +0200

    cleanup(neovim): remove dashboard

--- neovim/.config/nvim/lua/addons/lsp.lua
+++ neovim/.config/nvim/lua/addons/lsp.lua
@@ -1,23 +1,26 @@
 return {
-  "neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
 
-  dependencies = {
-    {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        },
-      },
-    }
-  },
+    dependencies = {
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        }
+    },
 
-  config = function()
-    vim.lsp.enable('lua_ls')
-    vim.lsp.enable('clangd')
-    vim.lsp.enable('pyright')
-  end
+    config = function()
+        vim.lsp.enable('lua_ls')
+        vim.lsp.enable('clangd')
+        vim.lsp.enable('pyright')
+        vim.lsp.enable('rust_analyzer')
+        vim.lsp.enable('metals')
+        vim.lsp.enable('zls')
+    end
 }
